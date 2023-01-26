a0e5c0ffe5d79cdf49256e731a7ffa096faa0a94
DIEGOHORVATTI <d.horvattid@gmail.com>
2023-01-26 12:10:05 -0300

    fix: atualização

--- src/interface/types.ts
+++ src/interface/types.ts
@@ -1,1 +1,32 @@
-idade
+import '../global'
+import { Body, Title } from '../function'
+
+/* 
+  Intersection types combina dois ou mais 
+  tipos, ou seja, um tipo que é composto
+*/
+
+Title('Intersection Types')
+interface Endereco {
+  rua: string
+  bairro: string
+  cidade: string
+}
+
+interface Pessoa {
+  nome: string
+  idade: number
+  profissao: string
+}
+
+type EnderecoPessoa = Endereco & Pessoa
+
+const IEnderecoPessoa: EnderecoPessoa = {
+  nome: 'Diego',
+  idade: 20,
+  profissao: 'Desenvolvidor',
+  rua: 'centro',
+  bairro: 'centro',
+  cidade: 'Videira - sc'
+}
+Body(IEnderecoPessoa)
